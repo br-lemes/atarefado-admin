7f260016c2e0db0fdf4657eb8ea8398f34033183
Breno Ramalho Lemes <breno@br-lemes.net>
2022-08-07 13:57:37 -0400

    Português (Brasil)

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import { Admin, Resource } from 'react-admin';
+import ptBrMessages from 'ra-language-pt-br';
+import polyglotI18nProvider from 'ra-i18n-polyglot';
+
 import PersonIcon from '@mui/icons-material/Person';
 import GroupIcon from '@mui/icons-material/Group';
 
@@ -6,8 +9,11 @@ import dataProvider from './localProvider';
 import { PerfilList, PerfilEdit, PerfilCreate } from './Perfil';
 import { UsuarioList, UsuarioCreate, UsuarioEdit } from './Usuario';
 
+const messages = { 'pt-br': ptBrMessages };
+const i18nProvider = polyglotI18nProvider(locale => messages[locale], 'pt-br');
+
 const App = () => (
-    <Admin dataProvider={dataProvider}>
+    <Admin dataProvider={dataProvider} i18nProvider={i18nProvider}>
         <Resource
             name='perfis'
             icon={GroupIcon}
